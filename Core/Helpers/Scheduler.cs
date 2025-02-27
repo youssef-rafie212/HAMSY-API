cca85cd3230f8f55feb8a0609ef2282f64170c37
MariamNB <nashaatmariam887@gmail.com>
2025-02-27 21:44:37 +0200

    🎉Completed HAMSY compiler implementation🎉

--- Core/Helpers/Scheduler.cs
+++ Core/Helpers/Scheduler.cs
@@ -7,90 +7,40 @@ namespace Core.Helpers
 {
 	public class Scheduler
 	{
-		private Dictionary<string, List<Instruction>> blocks = new();
+		private List<string> instructions;
+		private List<string> scheduledInstructions;
 
 		public Scheduler(List<string> assembly)
 		{
-			ParseBlocks(assembly);
-			foreach (var block in blocks.Keys.ToList())
-			{
-				BuildDependencyGraph(block);
-				ComputeCriticalPaths(block);
-				ScheduleBlock(block);
-			}
+			scheduledInstructions = new List<string>();
+			instructions = assembly;
 		}
 
-		private void ParseBlocks(List<string> assembly)
+		public List<string> GetScheduledCode()
 		{
-			string currentBlock = "global";
-			blocks[currentBlock] = new List<Instruction>();
-
-			foreach (var line in assembly)
-			{
-				if (line.EndsWith(":"))
-				{
-					currentBlock = line.TrimEnd(':');
-					blocks[currentBlock] = new List<Instruction>();
-				}
-				else
-				{
-					blocks[currentBlock].Add(new Instruction(line));
-				}
-			}
-		}
-
-		private void BuildDependencyGraph(string blockName)
-		{
-			var instructions = blocks[blockName];
-			for (int i = 0; i < instructions.Count; i++)
-			{
-				var instruction1 = instructions[i];
-				var parts1 = instruction1.Code.Split("->", StringSplitOptions.TrimEntries);
-				if (parts1.Length < 2) continue;
-				string destOperand = parts1[1];
-
-				for (int j = i + 1; j < instructions.Count; j++)
-				{
-					var instruction2 = instructions[j];
-					string[] srcOperands = instruction2.Code.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
-
-					if (srcOperands.Contains(destOperand))
-					{
-						instructions[i].Successors.Add(instruction2);
-						instructions[j].DepCount++;
-					}
-				}
-			}
-		}
-
-		private void ComputeCriticalPaths(string blockName)
-		{
-			var memo = new Dictionary<string, int>();
-			var instructions = blocks[blockName];
+			int currCycle = 0;
+			int latency = 1;
+			int cntr = 0;
 
 			foreach (var instr in instructions)
 			{
-				instr.Latency = GetLatency(instr.Code);
-				instr.CriticalPath = ComputeCriticalPath(instr, memo);
+				scheduledInstructions.Add($"{currCycle}: {instr}");
+				latency = GetLatency(instr);
+				cntr = currCycle + latency;
+				currCycle++;
+				while (currCycle < cntr)
+				{
+					scheduledInstructions.Add($"{currCycle}: stall");
+					currCycle++;
+				}
 			}
-		}
 
-		private int ComputeCriticalPath(Instruction instr, Dictionary<string, int> memo)
-		{
-			if (memo.TryGetValue(instr.Code, out int cachedPath))
-				return cachedPath;
-
-			if (instr.Successors.Count == 0)
-				return instr.Latency;
-
-			int criticalPath = instr.Latency + instr.Successors.Max(s => ComputeCriticalPath(s, memo));
-
-			memo[instr.Code] = criticalPath;
-			return criticalPath;
+			return scheduledInstructions;
 		}
 
 		private int GetLatency(string code)
 		{
+			if (code.StartsWith("loadI")) return 1;
 			if (code.StartsWith("load")) return 3;
 			if (code.StartsWith("store")) return 2;
 			if (code.StartsWith("add") || code.StartsWith("sub")) return 1;
@@ -100,57 +50,5 @@ namespace Core.Helpers
 			return 1;
 		}
 
-		private void ScheduleBlock(string blockName)
-		{
-			var instructions = blocks[blockName];
-			List<Instruction> readyList = instructions.Where(i => i.DepCount == 0)
-													   .OrderByDescending(i => i.CriticalPath)
-													   .ToList();
-			List<Instruction> activeList = new();
-			List<string> scheduledInstructions = new();
-			int curCycle = 0;
-
-			while (readyList.Any() || activeList.Any())
-			{
-				if (readyList.Any())
-				{
-					var op = readyList.First();
-					readyList.RemoveAt(0);
-					op.Cycle = curCycle;
-					activeList.Add(op);
-					scheduledInstructions.Add($"{curCycle} : {op.Code}");
-				}
-				else
-				{
-					scheduledInstructions.Add($"{curCycle} : stall");
-				}
-
-				curCycle++;
-				var toRemove = activeList.Where(op => op.Cycle + op.Latency == curCycle).ToList();
-
-				foreach (var op in toRemove)
-				{
-					activeList.Remove(op);
-					foreach (var succ in op.Successors)
-					{
-						succ.DepCount--;
-						if (succ.DepCount == 0)
-							readyList.Add(succ);
-					}
-				}
-			}
-			blocks[blockName] = scheduledInstructions.Select(s => new Instruction(s)).ToList();
-		}
-
-		public List<string> GetScheduledCode()
-		{
-			List<string> finalCode = new();
-			foreach (var block in blocks)
-			{
-				finalCode.Add(block.Key + ":");
-				finalCode.AddRange(block.Value.Select(instr => instr.Code));
-			}
-			return finalCode;
-		}
 	}
 }
--- HAMSY_API/Program.cs
+++ HAMSY_API/Program.cs
@@ -1,27 +1,28 @@
 using Core.ServiceContracts;
 using Core.Services;
 using HAMSY_API.Middlewares;
+using static HAMSY_API.Middlewares.GlobalExceptionHandler;
 
 namespace HAMSY_API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-            builder.Services.AddScoped<ICompilationService, CompilationService>();
-            builder.Services.AddScoped<IOCRService, OCRService>();
-            builder.Services.AddScoped<IMemoryAnalysisService, MemoryAnalysisService>();
-            builder.Services.AddScoped<IAIOptimizationService, AIOptimizationService>();
+			builder.Services.AddControllers();
+			builder.Services.AddScoped<ICompilationService, CompilationService>();
+			builder.Services.AddScoped<IOCRService, OCRService>();
+			builder.Services.AddScoped<IMemoryAnalysisService, MemoryAnalysisService>();
+			builder.Services.AddScoped<IAIOptimizationService, AIOptimizationService>();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            app.UseMiddleware<GlobalExceptionHandler>();
-            app.MapControllers();
+			app.UseMiddleware<GlobalExceptionMiddleware>();
+			app.MapControllers();
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
